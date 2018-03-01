b54dd74e5552935b8174048778933efbbbb9eed9
Sweta Rai <36945801+swetarai@users.noreply.github.com>
2018-03-01 12:54:39 +0530

    Update default.php

--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -3,7 +3,11 @@
 <head>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
-  	<title>Language Ci</title>
+  	<meta charset="UTF-8">
+  	<meta name="description" content="Demo of multiple language in codeigniter">
+  	<meta name="keywords" content="Multiple Language Codeigniter">
+  	<meta name="author" content="Sweta Rai">
+	<title>Language Ci</title>
 </head>
 <body>
 		<?php echo $content; ?>
